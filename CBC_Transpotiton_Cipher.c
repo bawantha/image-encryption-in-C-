51ff71fd0688f50b515b2cc7f3a52a56a85cce73
bawantha <bawwa.16@cse.mrt.ac.lk>
2019-02-03 20:10:02 +0530

    seems ok

--- CBC_Transpotiton_Cipher.c
+++ CBC_Transpotiton_Cipher.c
@@ -6,81 +6,106 @@
 // 160509B
 //
 
-
-
 // Define constants
 
 #define INPUT_FILE_NAME "watercolor.ppm"
 #define OUTPUT_FILE_NAME_ECB "enc_watercolor_ecb.ppm"
 #define OUTPUT_FILE_NAME_CBC "enc_watercolor_CBC.ppm"
 #define BLOCK_SIZE 24
-const int  KEY[]={10,
-10,
-17,
- 3,
- 9,
-23,
-18,
- 7,
-15,
- 1,
- 0,
-21,
-19,
-16,
-12,
- 2,
- 8,
- 6,
- 5,
-20,
-22,
-13,
- 4,
-14};        // Transpotiton Key
+const int KEY[] = {10,
+                   10,
+                   17,
+                   3,
+                   9,
+                   23,
+                   18,
+                   7,
+                   15,
+                   1,
+                   0,
+                   21,
+                   19,
+                   16,
+                   12,
+                   2,
+                   8,
+                   6,
+                   5,
+                   20,
+                   22,
+                   13,
+                   4,
+                   14}; // Transpotiton Key
 
-
-
-int main(){
+int main()
+{
     char in[BLOCK_SIZE];
     char ciper[BLOCK_SIZE];
     char header[54];
-    FILE *fp,*fo;
-    fp=fopen("watercolor.ppm","rb");
-    fo=fopen("watercolor_ECB.ppm","wb");
-    
+    char xor [BLOCK_SIZE];
+    char initializeVector[BLOCK_SIZE] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+    int start = 1;
+    char preCipherBlock[BLOCK_SIZE];
+
+    FILE *fp, *fo;
+    fp = fopen("watercolor.ppm", "rb");
+    fo = fopen("watercolor_ECB.ppm", "wb");
 
     // header value is taken
-    int header_read=fread(header,sizeof(char),54,fp);
+    int header_read = fread(header, sizeof(char), 54, fp);
 
     // header value is written to output
-    fwrite(header,sizeof(char),54,fo);
+    fwrite(header, sizeof(char), 54, fo);
 
- 
-
-    
-    while(1){
-        int read= fread(in,sizeof(char),BLOCK_SIZE,fp);
-        if(read<BLOCK_SIZE){
-            // add zeros to back for length is less than 24 block
-            for(int i=read;i<BLOCK_SIZE;i++){
-                in[i]=0;
+    while (start)
+    {
+        int read = fread(in, sizeof(char), BLOCK_SIZE, fp);
+        if (start == 1)
+        {
+            for (int i = 0; i < BLOCK_SIZE; i++)
+            {
+                xor[i] = in[i] ^ initializeVector[i];
             }
-            break;
+
+            for (int i = 0; i < BLOCK_SIZE; ++i)
+            {
+                int temp = in[i];
+                in[i] = in[KEY[i]];
+                in[KEY[i]] = temp;
+            }
+            for (int i = 0; i < BLOCK_SIZE; i++)
+            {
+                preCipherBlock[i] = in[i];
+            }
+            start++;
         }
-        for(int i=0;i<BLOCK_SIZE;++i){
-        int temp=in[i];
-        in[i]=in[KEY[i]];
-        in[KEY[i]]=temp;
-        
-    }
-    fwrite(in,sizeof(char),BLOCK_SIZE,fo);
+        else
+        {
+            for (int i = 0; i < BLOCK_SIZE; i++)
+            {
+                xor[i] = in[i] ^ preCipherBlock[i];
+            }
+
+            if (read < BLOCK_SIZE)
+            {
+                // add zeros to back for length is less than 24 block
+                for (int i = read; i < BLOCK_SIZE; i++)
+                {
+                    in[i] = 0;
+                }
+                break;
+            }
+            for (int i = 0; i < BLOCK_SIZE; ++i)
+            {
+                int temp = in[i];
+                in[i] = in[KEY[i]];
+                in[KEY[i]] = temp;
+            }
+        }
+        for (int i = 0; i < BLOCK_SIZE; i++)
+        {
+            preCipherBlock[i] = in[i];
+        }
+        fwrite(in, sizeof(char), BLOCK_SIZE, fo);
     }
 }
-    
-
-
-    
-   
-
-
